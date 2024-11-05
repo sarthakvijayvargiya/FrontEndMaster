4f2fe22e52d743370ab03c756b30466d0d0574c6
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2024-11-05 14:35:26 +0530

    feat: Initilaize and push

---
+++ JS/practice/jsprac/script.js
@@ -0,0 +1,11 @@
+// console.log(an);
+// console.log(asquare);
+let an = 10;
+
+const asquare = (num) => {
+  let ans = num * num;
+  return ans;
+};
+
+let asquare2 = asquare(2);
+let asquare4 = asquare(4);
