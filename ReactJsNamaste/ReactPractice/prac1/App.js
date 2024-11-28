6a72782dac3e52e14720aef936a8a3287466fa9a
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2024-11-28 23:03:01 +0530

    feat: adding new items

--- ReactJsNamaste/ReactPractice/prac1/App.js
+++ ReactJsNamaste/ReactPractice/prac1/App.js
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-// creating element is core feature of react
+import React from 'react'
 
-const heading = React.createElement("h1", {}, "Hellow World from React");
-//creating root from DOM maupilation is for browser so we use ReactDOM
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
---
+++ ReactJsNamaste/ReactPractice/prac1/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// creating element is core feature of react
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
